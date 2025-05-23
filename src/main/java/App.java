f328f6002570785fa5ca675ec404bb8511c48a26
SeongChoel <tldtlddl1234@gmail.com>
2025-05-23 17:36:27 +0900

    [BE8]김성철

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,27 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-<<<<<<< HEAD
-=======
-
->>>>>>> d798ed1 ([BE8]김성철)
     ArrayList<MyClass> list = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
     int num = 1;
 
-<<<<<<< HEAD
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-=======
-    void run() {
-        System.out.println("== 명언 앱 ==");
->>>>>>> d798ed1 ([BE8]김성철)
         while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
 
-<<<<<<< HEAD
             if (command.equals("종료")) break;
             else if (command.equals("등록")) register();
             else if (command.equals("목록")) showList();
@@ -32,7 +22,7 @@ public class App {
         }
     }
 
-    private void register() {
+    public void register() {
         System.out.print("명언 : ");
         String content = sc.nextLine().trim();
         System.out.print("작가 : ");
@@ -42,7 +32,7 @@ public class App {
         num++;
     }
 
-    private void showList() {
+    public void showList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------");
         for (int i = list.size() - 1; i >= 0; i--) {
@@ -51,7 +41,7 @@ public class App {
         }
     }
 
-    private void delete(String command) {
+    public void delete(String command) {
         int id = Integer.parseInt(command.substring(6));
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).id == id) {
@@ -63,7 +53,7 @@ public class App {
         System.out.println(id + "번 명언은 존재하지 않습니다.");
     }
 
-    private void update(String command) {
+    public void update(String command) {
         int id = Integer.parseInt(command.substring(6));
 
         for (MyClass m : list) {
@@ -82,32 +72,6 @@ public class App {
         }
         System.out.println(id + "번 명언은 존재하지 않습니다.");
     }
-=======
-            if (command.equals("종료"))
-                break;
-            else if (command.equals("등록")) {
-                register();
-            } else if (command.equals("목록")) {
-                showlist();
-            } else if (command.contains("삭제?id=")) {
-                delete(command);
-            } else if (command.contains("수정?id=")) {
-                update(command);
-            } else
-                System.out.println("다시 입력해주세요");
-        }
-
-    }
-
-    void register() {
-        System.out.print("명언 : ");
-        String context = sc.nextLine().trim();
-        System.out.print("작가 : ");
-        String author = sc.nextLine().trim();
-        System.out.println(num + "번 명언이 등록되었습니다.");
-        list.add(new MyClass(num, context, author));
-        num++;
-    }
 
     void showlist() {
         System.out.println("번호 / 작가 / 명언");
@@ -116,46 +80,4 @@ public class App {
             System.out.println(list.get(i).id + " / " + list.get(i).author + " / " + list.get(i).content);
     }
 
-    void delete(String command) {
-        boolean flag = false;
-        //int del = Integer.parseInt(command.substring(command.indexOf("=")+1));
-        int del = Integer.parseInt(command.substring(6));
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).id == del) {
-                list.remove(i);
-                System.out.println(del + "번 명언이 삭제되었습니다.");
-                flag = true;
-                break;
-            }
-        }
-        if (!flag)
-            System.out.println(del + "번 명언은 존재하지 않습니다.");
-    }
-
-    void update(String command) {
-        boolean flag = false;
-        //int renum = Integer.parseInt(command.substring(command.indexOf("=")+1));
-        int renum = Integer.parseInt(command.substring(6));
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).id == renum) {
-                System.out.println("명언(기존) : " + list.get(i).content);
-                System.out.print("명언 : ");
-                String content2 = sc.nextLine().trim();
-
-                System.out.println("작가(기존) :" + list.get(i).author);
-                System.out.print("작가 : ");
-                String author2 = sc.nextLine().trim();
-
-                list.get(i).content = content2;
-                list.get(i).author = author2;
-                flag = true;
-            }
-        }
-
-        if (!flag)
-            System.out.println(renum + "번 명언은 존재하지 않습니다.");
-    }
-
-
->>>>>>> d798ed1 ([BE8]김성철)
 }
